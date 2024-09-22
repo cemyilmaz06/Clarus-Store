a2b01a011d97a7b94c2e7519df18b0de71c2b32f
cem <cemyilmaztech@gmail.com>
2024-09-22 22:18:14 +0300

    productDetail component

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -6,6 +6,7 @@ import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 
 import PrivateRouter from "./PrivateRouter";
+import ProductDetail from "../pages/ProductDetail";
 
 const AppRouter = () => {
   return (
@@ -16,6 +17,7 @@ const AppRouter = () => {
         <Route path="" element={<PrivateRouter/>}>
         <Route path="/dashboard" element={<Home />} />
         <Route path="/dashboard/products" element={<Products />} />
+        <Route path="/dashboard/products/:id" element={<ProductDetail />} />
         <Route path="/dashboard/about" element={<About />} />
         </Route>
         <Route path="*" element={<NotFound />} />
