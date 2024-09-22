e754033061d5940232eae71f349bec8c94a9574a
cem <cemyilmaztech@gmail.com>
2024-09-22 20:35:03 +0300

    Products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,13 +1,14 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard'
 
 const Products = () => {
   const [products, setProducts] = useState([])
   const [search, setSearch] = useState("")
   const getData=async()=>{
     try {
-     const res= await axios.get(`https://dummyjson.com/products/search?q=${search}`);
-     console.log(res);
+     const {data}= await axios.get(`https://dummyjson.com/products/search?q=${search}`);
+     setProducts(data.products);
     } catch (error) {
       console.log(error);
     }
@@ -16,7 +17,14 @@ const Products = () => {
     getData();
   }, []);
   return (
-    <div>Products</div>
+    <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+      <h2 className='text-2xl font-bold mt-8 tracking-tight text-gray-900'>All Products</h2>
+      <div className='mt-6 grid grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10 xl:gap-x-8'>
+      {products.map((product)=>(
+<ProductCard key={product.id} product={product} />
+      ))}
+      </div>
+    </div>
   )
 }
 
